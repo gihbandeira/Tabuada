e0ffc95fed4d2be3b8fbcb6f733889fc730bc5f4
matheuspereira123 <matheuspghc1@gmail.com>
2020-03-11 20:27:37 -0300

    Tabuada_2020.03.11

---
+++ 2020.03.11/tabuada.php
@@ -0,0 +1,18 @@
+<?php
+    $tabuada = $_REQUEST ["tabuada"];
+
+    if($tabuada <=0 || $tabuada > 50){
+        echo "Valor invalido para tabuada";
+        exit;
+    }
+
+    $arrayJson = array();
+    $arrayJson["tabuada"] = $tabuada;
+
+    for($contador = 0; $contador <=10; $contador++){
+        $arrayJson[$tabuada . "X" . $contador] = $tabuada * $contador;
+    }
+
+    echo json_encode($arrayJson);
+
+?>
